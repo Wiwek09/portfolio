b5298140c5af48bda32559f0c9b9b04a70224b82
Wiwek09 <bibekwiwek54@gmail.com>
2023-03-06 22:09:58 +0545

    tried decreasing font sies and icon to make responsive

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ import cartoon from 'public/cartoon.png'
 
 export default function Home() {
   return (
-    <div className='bg-white'>
+    <div className='bg-white '>
       <section className= 'min-h-screen' > {/*Section from main heading to image container */}
-        <div className=' bg-gray-100' >
+        <div className=' bg-yellow-200' >
          <nav className='py-10 p-10 mb-10 flex justify-between '>
           <h1 className='text-xl font-burtons'>Er.Wiwek</h1>
           <ul className='flex item-center'>
@@ -21,14 +21,14 @@ export default function Home() {
           </ul>
          </nav>
          <div className='text-center p-10 flex flex-col'>
-          <h2 className='text-6xl py-2 text-teal-600 font-extrabold '>Wiwek_K.C.</h2>
-          <h3 className='text-3xl py-2'>Software Developer and Data Analyst</h3>
-          <p className='text-md py-5 leading-8 text-gray-800 font-serif' >
+          <h2 className='md:text-6xl text-5xl py-2 text-teal-600 font-extrabold '>Wiwek_K.C.</h2>
+          <h3 className='md:text-3xl text-xl py-2'>Software Developer and Data Analyst</h3>
+          <p className='md:text-md  py-5 leading-8 text-gray-800 font-serif' >
             This is me a software developer and data insight analyst who is interested in technology and its impacts on business corporates industry and in consumer level.
           </p>
          </div>
         </div>
-         <div className='text-5xl flex justify-center gap-16 py-1 text-gray-700'>
+         <div className='md:text-5xl text-4xl flex justify-center md:gap-16 gap-10 py-1 text-gray-700'>
           <AiFillTwitterCircle />
           <AiFillLinkedin/>
           <AiFillInstagram/>
@@ -38,89 +38,89 @@ export default function Home() {
           <Image src={cartoon} layout='fill' objectFit='cover'  />
          </div>
       </section>
-      <section className='p-10 bg-gray-100 flex flex-col md:flex-row gap-6 min-h-fit'> {/*Section of about me and skills*/}
+      <section className='p-10 bg-yellow-100 flex flex-col md:flex-row gap-6 min-h-fit'> {/*Section of about me and skills*/}
         <div className='w-full md:w-[50%]'>
            <h3 className='text-3xl font-extrabold text-teal-700 uppercase'>About Me</h3>
-           <p className='text-md leading-8 text-gray-800 text-justify mt-2 italic hover:not-italic'>
+           <p className='text-md leading-8 text-gray-800 text-justify mt-2 '>
               I am a <span className='text-teal-500'> MERN developer </span> and <span className='text-teal-500'>Analytics engineer </span> who loves to try new tech stack. I have done some projects regarding <span className='text-teal-500'>desktop</span> and <span className='text-teal-500'> web application </span>. I have a passion for working in web technology,solving problems and thrive in a team environment.
            </p>
-           <p className='text-md leading-8 text-gray-800 pt-6 italic hover:not-italic'>
+           <p className='text-md leading-8 text-gray-800 pt-6'>
               I'm open to <span className='text-teal-500'> Job</span> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to <span className='text-teal-500'>contact</span> me.
            </p>
         </div>
-        <div className='w-full ml-12 md:w-[50%] flex flex-col'>
-          <h3 className='text-3xl font-extrabold text-teal-700 uppercase'>Technology I have worked with</h3>
-          <section className=' text-2xl text-yellow-600 mt-2 font-serif' >1. WEB APPLICATION
-          <ul className='grid grid-cols-4 gap-y-2 mt-1'>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black'>
+        <div className='w-full md:ml-12 md:w-[50%]'>
+          <h3 className='md:text-3xl text-2xl font-extrabold text-teal-700 uppercase'>Technology I have worked with</h3>
+          <section className=' md:text-2xl text-l text-green-600 mt-2 font-serif' >1. WEB APPLICATION
+          <ul className='grid md:grid-cols-4 md:gap-y-2 mt-1 grid-cols-3 gap-4'>
+            <li className=' list '>
               HTML
             </li>
-            <li className= 'bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className= 'list '>
               CSS
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Javascript
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               React
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Next
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Node
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Express
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Tailwind
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               PostCSS
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               MongoDB
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               RestAPI
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               GraphQL
             </li>
           </ul>
           </section>
-          <section className=' text-2xl text-yellow-600 mt-4 font-serif' >2. DESKTOP APPLICATION
-          <ul className='grid grid-cols-5 gap-y-2 mt-1'>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black'>
+          <section className=' md:text-2xl text-l text-green-600 mt-4 font-serif text-clip' >2. DESKTOP APPLICATION
+          <ul className='grid md:grid-cols-5 md:gap-y-2 mt-1 grid-cols-4 gap-4'>
+            <li className=' list'>
               Python
             </li>
-            <li className= 'bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className= 'list '>
               Tkinter
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               SQlite
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               MySQL
             </li>
             </ul>
           </section>
-          <section className=' text-2xl text-yellow-600 mt-4 font-serif' >3. DATA VISUALIZATION
-          <ul className='grid grid-cols-5 gap-y-2 mt-1'>
-          <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black'>
+          <section className=' md:text-2xl text-l text-green-600 mt-4 font-serif' >3. DATA VISUALIZATION
+          <ul className='grid md:grid-cols-5 md:gap-y-2 mt-1 grid-cols-3 gap-4'>
+          <li className=' list'>
               Python
             </li>
-            <li className= 'bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className= 'list '>
               NumPy
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Pandas
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               MatPlotLib
             </li>
-            <li className=' bg-green-300 rounded-xl w-fit h-fit text-lg text-black '>
+            <li className=' list '>
               Seaborn
             </li>
           </ul>
@@ -132,7 +132,7 @@ export default function Home() {
         <h1 className='text-3xl font-extrabold text-teal-700 text-center' >My Works</h1>
         <div className=' bg-teal-700 h-1 w-8  rounded-full m-auto text-center' ></div>
         <div className='flex justify-center'>
-        <p className='w-full m-auto  text-center text-lg font-serif'>Here you will find some of the personal projects that I created with each project containing all of the tech stacks used</p>
+        <p className='w-full m-auto  text-center text-lg font-serif'>Here you will find some of the personal projects that I created, with each project containing all of the tech stacks used</p>
         </div>
         
        </main>
